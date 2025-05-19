f951f7e77dfb4cb0d41ce7039ad5edaf5b0d4108
Ritik2712 <ritik27122mahajan@gmail.com>
2025-05-19 23:48:42 +0530

    modified wrangler

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 module.exports = nextConfig;
